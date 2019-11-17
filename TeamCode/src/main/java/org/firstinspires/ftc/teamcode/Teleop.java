9d9465ae89770c25132624b133f00f206f18ccf4
Ansh Gandhi <ansh.goodboy@gmail.com>
2019-11-16 18:00:27 -0800

    added robot class to initlize entire robot

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Teleop.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Teleop.java
@@ -1,24 +1,15 @@
 package org.firstinspires.ftc.teamcode;
 
-import com.qualcomm.hardware.rev.Rev2mDistanceSensor;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.util.ElapsedTime;
 
-import org.firstinspires.ftc.robotcore.external.navigation.DistanceUnit;
-import org.firstinspires.ftc.teamcode.RobotClasses.Clamp;
-import org.firstinspires.ftc.teamcode.RobotClasses.Intake;
-import org.firstinspires.ftc.teamcode.RobotClasses.MecanumDrivetrain;
-import org.firstinspires.ftc.teamcode.RobotClasses.Deposit;
+import org.firstinspires.ftc.teamcode.RobotClasses.Robot;
 
 @TeleOp(name="Teleop") @SuppressWarnings("FieldCanBeLocal")
 public class Teleop extends LinearOpMode {
 
-    private MecanumDrivetrain drivetrain;
-    private Intake intake;
-    private Deposit deposit;
-    private Clamp clamp;
-    private Rev2mDistanceSensor stoneSensor;
+    private Robot robot;
 
     private double forward = 0;
     private double right = 0;
@@ -31,23 +22,17 @@ public class Teleop extends LinearOpMode {
 
     @Override
     public void runOpMode() {
-        drivetrain = new MecanumDrivetrain(hardwareMap,this,0,0,0);
-        intake = new Intake(hardwareMap,this);
-        deposit = new Deposit(hardwareMap, this);
-        clamp = new Clamp(hardwareMap,this);
-        stoneSensor = hardwareMap.get(Rev2mDistanceSensor.class, "stoneSensor");
-        
+        robot = new Robot(hardwareMap, this);
+        robot.deposit.unclampStone();
+        robot.clamp.openClamp();
         ElapsedTime xBuffer = new ElapsedTime();
 
-        deposit.clampStone();
-        clamp.openClamp();
-
         waitForStart();
-        drivetrain.resetAngle();
-        intake.setControls(1);
+        robot.drivetrain.resetAngle();
+        robot.intake.setControls(1);
 
         while(opModeIsActive()){
-            angle = drivetrain.getAngle();
+            angle = robot.drivetrain.getAngle();
             if (robotCentric) {
             forward = gamepad1.left_stick_y;
             right = gamepad1.left_stick_x;
@@ -68,26 +53,20 @@ public class Teleop extends LinearOpMode {
                 xBuffer.reset();
                 xBuffer.startTime();
             }
-    
-            if (stoneSensor.getDistance(DistanceUnit.MM) <= 5) {
-                telemetry.addData("stone", "detected");
-                deposit.clampStone();
-            }
 
-            if (gamepad1.a) clamp.openClamp();
-            else if (gamepad1.b) clamp.closeClamp();
+            if (gamepad1.a) robot.clamp.openClamp();
+            else if (gamepad1.b) robot.clamp.closeClamp();
             clampPower = gamepad1.right_trigger*0.25 - gamepad1.left_trigger*0.25;
 
-            drivetrain.setControls(right, forward, gamepad1.right_stick_x); drivetrain.updatePose();
-            intake.setControls(intakePower);
-            deposit.setControls(transferPower);
-            clamp.setControls(clampPower);
+            robot.drivetrain.setControls(right, forward, gamepad1.right_stick_x); robot.drivetrain.updatePose();
+            robot.intake.setControls(intakePower);
+            robot.deposit.setControls(transferPower);
+            robot.clamp.setControls(clampPower);
 
-            telemetry.addData("X", drivetrain.x);
-            telemetry.addData("Y", drivetrain.y);
-            telemetry.addData("Theta", drivetrain.currentheading);
+            telemetry.addData("X", robot.drivetrain.x);
+            telemetry.addData("Y", robot.drivetrain.y);
+            telemetry.addData("Theta", robot.drivetrain.currentheading);
             telemetry.addData("Heading", angle);
-            telemetry.addData("Distance", String.format("%.01f mm", stoneSensor.getDistance(DistanceUnit.MM)));
             telemetry.addData("stone", "not detected");
             telemetry.update();
         }
