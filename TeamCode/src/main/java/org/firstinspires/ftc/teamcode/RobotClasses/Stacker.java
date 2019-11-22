63d52d84aeb029da815a59b80936a423d7cbd3ea
Jonathan Ma <jonathanjma03@gmail.com>
2019-11-21 16:31:39 -0800

    added centric switcher, manual clamp and unclamp button adjusted stacker encoder values, fixed index out of bounds error

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/RobotClasses/Stacker.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/RobotClasses/Stacker.java
@@ -8,7 +8,7 @@ import com.qualcomm.robotcore.hardware.Servo;
 
 @SuppressWarnings("FieldCanBeLocal")
 public class Stacker {
-
+    
     //Electronics
     private DcMotorEx liftMotor;
     private DcMotorEx depositMotor;
@@ -16,22 +16,21 @@ public class Stacker {
     
     private final double clampPos = 0.9;
     private final double unClampPos = 0.65;
-
-
-    private final int armpos[] = {2160,1990,1700,1560,1260,1260,1260};
-    private final int liftpos[] = {0,0,0,0,250,780,1250};
-
+    
+    private final int armPos[] =  {2160, 1970, 1700, 1550, 1250, 1250, 1250}; // increases down
+    private final int liftPos[] = {0,    0,    0,    0,    280,  810,  1280};
+    
     private int currentStackHeight = 0;
-
+    
     //OpMode Stuff
     private LinearOpMode op;
     private HardwareMap hardwareMap;
-
-    public Stacker(LinearOpMode op){
-
+    
+    public Stacker(LinearOpMode op) {
+        
         this.op = op;
         this.hardwareMap = op.hardwareMap;
-
+        
         liftMotor = hardwareMap.get(DcMotorEx.class, "liftMotor");
         depositMotor = hardwareMap.get(DcMotorEx.class, "depositMotor");
         stoneClamp = hardwareMap.get(Servo.class, "stoneClamp");
@@ -41,7 +40,7 @@ public class Stacker {
         
         liftMotor.setTargetPosition(0);
         liftMotor.setMode(DcMotor.RunMode.RUN_TO_POSITION);
-    
+        
         depositMotor.setTargetPosition(0);
         depositMotor.setMode(DcMotor.RunMode.RUN_TO_POSITION);
         
@@ -51,43 +50,47 @@ public class Stacker {
         op.telemetry.addData("Status", "Stacker Initialized");
         op.telemetry.update();
     }
-    public void setLiftControls(double power, int ticks){
+    
+    public void setLiftControls(double power, int ticks) {
         liftMotor.setMode(DcMotor.RunMode.RUN_TO_POSITION);
         liftMotor.setPower(power);
         liftMotor.setTargetPosition(ticks);
     }
-    public void setDepositControls(double power, int ticks){
+    
+    public void setDepositControls(double power, int ticks) {
         depositMotor.setMode(DcMotor.RunMode.RUN_TO_POSITION);
         depositMotor.setPower(power);
         depositMotor.setTargetPosition(ticks);
     }
     
-    public void setDepositPower(double power){
+    public void setDepositPower(double power) {
         depositMotor.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
         depositMotor.setPower(power);
-        
     }
-    public void setLiftPower(double power){
+    
+    public void setLiftPower(double power) {
         liftMotor.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
         liftMotor.setPower(power);
     }
-    public void deposit(){
-        setLiftControls(1, liftpos[currentStackHeight]);
-        setDepositControls(0.75, armpos[currentStackHeight]);
+    
+    public void deposit() {
+        setLiftControls(1, liftPos[currentStackHeight]);
+        setDepositControls(0.75, armPos[currentStackHeight]);
     }
-
-
-    public void nextLevel(){
-        currentStackHeight++;
+    
+    
+    public void nextLevel() {
+        currentStackHeight = Math.min(currentStackHeight + 1, 6);
     }
-    public void lastLevel(){
-        currentStackHeight--;
+    
+    public void lastLevel() {
+        currentStackHeight = Math.max(currentStackHeight - 1, 0);
     }
     
     public void clampStone() {
         stoneClamp.setPosition(clampPos);
     }
-
+    
     public void unClampStone() {
         stoneClamp.setPosition(unClampPos);
     }
@@ -95,6 +98,7 @@ public class Stacker {
     public double getLiftPosition() {
         return liftMotor.getCurrentPosition();
     }
+    
     public double getArmPosition() {
         return depositMotor.getCurrentPosition();
     }
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Teleop.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Teleop.java
@@ -2,119 +2,106 @@ package org.firstinspires.ftc.teamcode;
 
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
-import com.qualcomm.robotcore.util.ElapsedTime;
 
 import org.firstinspires.ftc.robotcore.external.navigation.DistanceUnit;
 import org.firstinspires.ftc.teamcode.RobotClasses.Robot;
 
-@TeleOp(name="Teleop") @SuppressWarnings("FieldCanBeLocal")
+@TeleOp(name = "Teleop")
+@SuppressWarnings("FieldCanBeLocal")
 public class Teleop extends LinearOpMode {
-
+    
     private Robot robot;
-
-    private double intakePower = 0;
-    private double armPower = 0;
-    private double liftPower = 0;
-
-    private final boolean robotCentric = false;
+    private boolean robotCentric = false;
     private int z = 0;
-    private int stackcounter = 0;
-    private boolean armout = false;
-
-    boolean dpadup = true;
-    boolean dpaddown = true;
-
+    private int stackCounter = 0;
+    private boolean armOut = false;
+    private int centricWait = 0;
+    private boolean dpadUp = true;
+    private boolean dpadDown = true;
+    
     @Override
     public void runOpMode() {
-        robot = new Robot(this, 0 , 0,0);
+        robot = new Robot(this, 0, 0, 0);
         //robot.stacker.unClampStone();
-        ElapsedTime xBuffer = new ElapsedTime();
-
+        
         waitForStart();
         robot.drivetrain.resetAngle();
         robot.intake.setControls(0);
         robot.stacker.unClampStone();
-        robot.stacker.setDepositControls(0.5,130);
-        while (opModeIsActive()){
-
-//            if (gamepad1.x && xBuffer.milliseconds()>300 && intakePower == 0) {
-//                intakePower = 1;
-//                xBuffer.reset();
-//                xBuffer.startTime();
-//            } else if (gamepad1.x && xBuffer.milliseconds()>300) {
-//                intakePower = 0;
-//                xBuffer.reset();
-//                xBuffer.startTime();
-//            }
-
-
-
+        robot.stacker.setDepositControls(0.5, 130);
+        while (opModeIsActive()) {
+            
             z++;
-            if(z%100 == 0 && stackcounter == 0){
+            if (z % 80 == 0 && stackCounter == 0) {
                 double distance = robot.stoneSensor.getDistance(DistanceUnit.INCH);
                 if (distance < 6) {
-                    robot.stacker.setDepositControls(0.5,50);
+                    robot.stacker.setDepositControls(0.5, 50);
                     robot.stacker.clampStone();
                     robot.intake.setControls(0);
-                }
-                else{
+                } else {
                     robot.intake.setControls(1);
                 }
             }
-            if(gamepad1.left_bumper && armout){
+            if (gamepad1.left_bumper && armOut) {
                 robot.stacker.unClampStone();
-                robot.stacker.setDepositControls(0.2, (int)robot.stacker.getArmPosition() + 70);
+                robot.stacker.setDepositControls(0.2, (int) robot.stacker.getArmPosition() + 70);
             }
-
-            if(gamepad1.right_bumper && stackcounter == 0 && !armout){
+            
+            if (gamepad1.right_bumper && stackCounter == 0 && !armOut) {
                 robot.stacker.deposit();
-                armout = true;
-                stackcounter = 100;
-            }else if(gamepad1.right_bumper && stackcounter == 0 && armout){
-                robot.stacker.setLiftControls(0.5,0);
-                robot.stacker.setDepositControls(0.5,130);
+                armOut = true;
+                stackCounter = 100;
+            } else if (gamepad1.right_bumper && stackCounter == 0 && armOut) {
+                robot.stacker.setLiftControls(0.5, 0);
+                robot.stacker.setDepositControls(0.5, 130);
                 robot.stacker.nextLevel();
-                stackcounter = 100;
-                armout = false;
+                stackCounter = 100;
+                armOut = false;
+            } else if (stackCounter > 0) {
+                stackCounter--;
             }
-            else if(stackcounter>0){
-                stackcounter--;
-            }
-
-
-            if(gamepad1.dpad_up && dpadup){
-                dpadup = false;
-
-            }else if(!dpadup && !gamepad1.dpad_up){
+            
+            if (gamepad1.dpad_up && dpadUp) {
+                dpadUp = false;
+                
+            } else if (!dpadUp && !gamepad1.dpad_up) {
                 robot.stacker.nextLevel();
-                dpadup = true;
+                dpadUp = true;
             }
-
-            if(gamepad1.dpad_down && dpaddown){
-                dpaddown = false;
-
-            }else if(!dpaddown && !gamepad1.dpad_down){
+            
+            if (gamepad1.dpad_down && dpadDown) {
+                dpadDown = false;
+                
+            } else if (!dpadDown && !gamepad1.dpad_down) {
                 robot.stacker.lastLevel();
-                dpaddown = true;
+                dpadDown = true;
             }
-
-
-
+            
+            if (gamepad1.x && centricWait == 0) {
+                robotCentric = !robotCentric;
+                centricWait = 100;
+            } else if (centricWait > 0) centricWait--;
+            
+            if (gamepad1.left_trigger > 0) {
+                robot.stacker.clampStone();
+            }
+            if (gamepad1.right_trigger > 0) {
+                robot.stacker.unClampStone();
+                robot.stacker.setDepositControls(0.5, 130);
+                robot.intake.setControls(-1);
+            }
             
             if (gamepad1.dpad_left) robot.grabber.grabFoundation();
             if (gamepad1.dpad_right) robot.grabber.releaseFoundation();
-
-    
+            
             if (robotCentric) {
                 robot.drivetrain.setControls(-gamepad1.left_stick_x, -gamepad1.left_stick_y, -gamepad1.right_stick_x);
+            } else {
+                robot.drivetrain.setGlobalControls(-gamepad1.left_stick_y, -gamepad1.left_stick_x, -gamepad1.right_stick_x);
             }
-            else {robot.drivetrain.setGlobalControls(-gamepad1.left_stick_y, -gamepad1.left_stick_x, -gamepad1.right_stick_x);}
             robot.update();
-
-
-//            telemetry.addData("X", robot.drivetrain.x);
-//            telemetry.addData("Y", robot.drivetrain.y);
-//            telemetry.addData("Theta", robot.drivetrain.currentheading);
+            
+            telemetry.addData("Robot Centric", robotCentric);
             telemetry.update();
         }
     }
