b6577ebd4caf708b33013c0fe9e1ee143bc53680
Jonathan Ma <jonathanjma03@gmail.com>
2019-11-22 18:23:12 -0800

    clean up, file save changes

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/RobotClasses/Robot.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/RobotClasses/Robot.java
@@ -7,139 +7,147 @@ import org.firstinspires.ftc.robotcore.external.navigation.DistanceUnit;
 
 import java.io.BufferedReader;
 import java.io.File;
+import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.FileWriter;
+import java.io.IOException;
 
 public class Robot {
 
-    //subsystems
+    // Subsystems
     public MecanumDrivetrain drivetrain;
     public Intake intake;
     public Stacker stacker;
     public FoundationGrabber grabber;
     public Rev2mDistanceSensor stoneSensor;
 
-    //state booleans
-    private boolean stoneInRobot = false;
+    // State booleans
+    public boolean stoneInRobot = false;
     private boolean armIsOut = false;
 
-    //TODO create clampedonFoundation methods
+    //TODO create clampedOnFoundation methods
     private boolean clampedOnFoundation = false;
     private boolean downstacked = false;
 
-
-    //class constants
+    // Class constants
     private final int stoneSensorUpdatePeriod = 20;
     private final int stoneValidationDistance = 6;
     private final int armTicksUpdatePeriod = 10;
 
     private int cycleCounter = 0;
-
     private int z = 20;
 
-    private static File robotPositionLog = new File(new File("/sdcard/FIRST/"), "RobotPositionLog");
-    
+    private static File robotPositionLog;
+
     public Robot(LinearOpMode op, double initX, double initY, double initTheta) {
         drivetrain = new MecanumDrivetrain(op, initX, initY, initTheta);
         intake = new Intake(op);
         stacker = new Stacker(op);
         grabber = new FoundationGrabber(op);
-        
-        stoneSensor = op.hardwareMap.get(Rev2mDistanceSensor.class, "stoneSensor");
 
+        stoneSensor = op.hardwareMap.get(Rev2mDistanceSensor.class, "stoneSensor");
     }
-    
+
     public void update() {
-        //increase cycle count
+        // increase cycle count
         cycleCounter++;
 
-        //update stoneInRobot boolean
-        if(cycleCounter%stoneSensorUpdatePeriod == 0){
+        // update stoneInRobot boolean
+        if (cycleCounter % stoneSensorUpdatePeriod == 0) {
             stoneInRobot = stoneSensor.getDistance(DistanceUnit.INCH) < stoneValidationDistance;
         }
 
-        //update arm is out boolean
-        if((cycleCounter+3)%armTicksUpdatePeriod == 0){
+        // update arm
+        if ((cycleCounter + 3) % armTicksUpdatePeriod == 0) {
             stacker.update();
         }
 
-        //check states
-
-        //check if ready to collect
-        if(!stoneInRobot && ishome()){
+        // check states----------------------
+        // check if ready to collect stones
+        if (!stoneInRobot && isHome()) {
             intake.setControls(1);
         }
-        //start clamping procedure
-        else if(stoneInRobot && ishome()){
-            if(z>0){
+        // check if arm should lower in preparation to clamp stone
+        else if (stoneInRobot && isHome()) {
+            if (z > 0) {
                 z--;
 
-            }else{
+            } else {
                 intake.setControls(0);
                 stacker.goDown();
                 z = 20;
             }
 
         }
-        else if(stoneInRobot && stacker.isArmDown() && !stacker.stoneClamped){
+        // check if stone should be clamped
+        else if (stoneInRobot && stacker.isArmDown() && !stacker.stoneClamped) {
             stacker.clampStone();
         }
-        else if(!stacker.isArmMoving() && downstacked){
+        // check if stone should be unclamped
+        else if (!stacker.isArmMoving() && downstacked) {
             stacker.unClampStone();
-            stacker.setLiftControls(0.8, Math.min(stacker.getLiftPosition()+100, 1285));
+            stacker.setLiftControls(0.8, Math.min(stacker.getLiftPosition() + 100, 1285));
             downstacked = false;
         }
 
         drivetrain.updatePose();
     }
-    public boolean ishome(){
+
+    public boolean isHome(){
         return (stacker.isArmHome() && stacker.isLiftHome() && !stacker.stoneClamped);
     }
-    public void swapArmState(){
-        if(!stacker.isArmOut()){
+
+    public void swapArmState() {
+        if (!stacker.isArmOut()) {
             stacker.deposit();
             stacker.nextLevel();
-        }
-        else if(!stacker.stoneClamped){
+        } else if (!stacker.stoneClamped) {
             stacker.goHome();
         }
     }
-    public void deposit(){
-        if(stacker.isArmOut() && stacker.stoneClamped){
+
+    public void deposit() {
+        if (stacker.isArmOut() && stacker.stoneClamped) {
             stacker.downStack();
             downstacked = true;
         }
     }
 
-
-
-
-    /*//file stuff
-    public void writePos(double x, double y, double theta) {
+    //file stuff
+    /*public void writePos(double x, double y, double theta) {
+        robotPositionLog = new File("/sdcard/FIRST/RobotPosition.txt");
         try {
             FileWriter fileWriter = new FileWriter(robotPositionLog);
             fileWriter.write(x + "\n");
             fileWriter.write(y + "\n");
-            fileWriter.write(theta + "\n");
-            fileWriter.write("\n");
+            fileWriter.write(theta + "");
             fileWriter.close();
         } catch (Exception e) {e.printStackTrace();}
     }
-    public static Double[] readPos() {
-        String line;
-        Double[] lines = new Double[3];
+
+    public static double[] readPos() {
+        String curLine;
+        double[] robotPos = new double[3];
+
         int counter = 0;
         try {
-            FileReader fileReader = new FileReader(robotPositionLog);
-            BufferedReader bufferedReader = new BufferedReader(fileReader);
+            BufferedReader bufferedReader = new BufferedReader(new FileReader(robotPositionLog));
 
-            while ((line = bufferedReader.readLine()) != null) {
-                lines[counter] = Double.parseDouble(line);
-                counter = (counter + 1) % 3;
-
+            while ((curLine = bufferedReader.readLine()) != null) {
+                robotPos[counter] = Double.parseDouble(curLine);
+                counter++;
             }
-            fileReader.close();
-        } catch (Exception e) {e.printStackTrace();}
-        return lines;
+            bufferedReader.close();
+        } catch (FileNotFoundException e) {
+            robotPos[0] = 0; robotPos[1] = 0; robotPos[2] = 0;
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return robotPos;
+    }
+
+    public static void deletePosFile() {
+        robotPositionLog.delete();
     }*/
 }
