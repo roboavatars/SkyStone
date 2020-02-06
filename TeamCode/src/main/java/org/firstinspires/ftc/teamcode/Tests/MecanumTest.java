1802c2aede97d76bbbbb10f8fb9674dac0adfa57
Jonathan Ma <jonathanjma03@gmail.com>
2020-02-05 21:35:36 -0800

    minor auto changes, no real improvement

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Tests/MecanumTest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Tests/MecanumTest.java
@@ -2,6 +2,8 @@ package org.firstinspires.ftc.teamcode.Tests;
 
 import android.util.Log;
 
+import com.acmerobotics.dashboard.FtcDashboard;
+import com.acmerobotics.dashboard.telemetry.TelemetryPacket;
 import com.qualcomm.robotcore.eventloop.opmode.Disabled;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
@@ -16,20 +18,21 @@ public class MecanumTest extends LinearOpMode {
     private ElapsedTime time = new ElapsedTime();
 
     @Override
-    public void runOpMode() throws InterruptedException {
+    public void runOpMode() {
         drivetrain = new MecanumDrivetrain(this,0,0,0, false);
+        FtcDashboard dashboard = FtcDashboard.getInstance();
 
         waitForStart();
 
         time.reset();
         while(opModeIsActive()){
             drivetrain.updatePose();
-            drivetrain.setControls(0,0.6,-0.35);
+            TelemetryPacket packet = new TelemetryPacket();
 
-            telemetry.addData("x", drivetrain.x);
-            telemetry.addData("y", drivetrain.y);
-            telemetry.addData("theta", drivetrain.currentheading);
-            telemetry.update();
+            packet.put("x", drivetrain.x);
+            packet.put("y", drivetrain.y);
+            packet.put("theta", drivetrain.currentheading);
+            dashboard.sendTelemetryPacket(packet);
 
             Log.w("auto", String.format("%.5f", drivetrain.x) + " " + String.format("%.5f", drivetrain.y) + " " + String.format("%.5f", drivetrain.currentheading));
         }
