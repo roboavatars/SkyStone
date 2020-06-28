f08fd1d7826c24c9dd0923d8810f4175d8df8529
Jonathan Ma <jonathanjma03@gmail.com>
2020-06-28 12:28:09 -0700

    varun and update build tools

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/VarunSummer/Test.java
@@ -0,0 +1,18 @@
+package org.firstinspires.ftc.teamcode.VarunSummer;
+
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+
+@TeleOp
+public class Test extends LinearOpMode {
+
+    @Override
+    public void runOpMode() {
+
+        waitForStart();
+
+        while(opModeIsActive()) {
+
+        }
+    }
+}
